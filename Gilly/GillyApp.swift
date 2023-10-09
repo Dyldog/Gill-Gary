c575b56969549dfdcf91e063d37d964975b5ba91
Dylan Elliott <dylan.elliott@gmail.com>
2023-10-09 21:03:59 +1100

    Initial Commit

---
+++ Gilly/GillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GillyApp.swift
+//  Gilly
+//
+//  Created by Dylan Elliott on 9/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
